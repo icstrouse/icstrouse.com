c842c510fe2ca7f2b2a0e223d18491a38ea0a0c6
Ian <ian.strouse@gmail.com>
2023-06-22 18:41:03 -0600

    1st draft

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,11 +12,7 @@ export const metadata = {
   description: 'My personal page & portfolio',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
